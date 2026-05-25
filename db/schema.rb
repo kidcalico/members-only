c7848b49747f980de4a49ca867ff8b518d0fe19f
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2026-05-25 13:25:14 -0700

    Update PostsController and generate devise views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_25_190330) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_25_200350) do
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
@@ -24,4 +33,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_25_190330) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
+
+  add_foreign_key "posts", "users"
 end
